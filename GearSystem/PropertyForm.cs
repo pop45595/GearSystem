a9dfbedae505cbce680cd740098ee0a5ea6856a0
Yang <repeatsa01@gmail.com>
2017-07-27 21:44:32 +0800

    PropertyForm

--- GearSystem/PropertyForm.cs
+++ GearSystem/PropertyForm.cs
@@ -5,43 +5,159 @@ namespace GearSystem
 {
     public class PropertyForm : IPropertyV2
     {
-        private Hashtable m_Hashtable = null;
-
-        public PropertyForm()
-        {
-            reset();
-        }
-
-        public bool checkProperty(eGearState _eKey)
-        {
-            return m_Hashtable.ContainsKey(_eKey);
-        }
-
         public IPropertyV2 copy()
         {
-            PropertyForm newProperty = new PropertyForm();
-            foreach(DictionaryEntry TheValue in m_Hashtable)
-            {
-                newProperty.setPropertyValue<object>((eGearState)TheValue.Key, TheValue.Value);
-            }
-
-            return newProperty;
+            throw new NotImplementedException();
         }
 
-        public T getPropertyValue<T>(eGearState _eKey)
+        public int 取攻擊(int _iValue)
         {
-            return (m_Hashtable.ContainsKey(_eKey)) ? (T)m_Hashtable[_eKey] : default(T);
+            throw new NotImplementedException();
         }
 
-        public void reset()
+        public int 取攻擊力加乘(int _iValue)
         {
-            m_Hashtable = new Hashtable();
+            throw new NotImplementedException();
         }
 
-        public bool setPropertyValue<T>(eGearState _eGearState, T _iValue)
+        public int 取攻擊速度(int _iValue)
         {
-            m_Hashtable.Add(_eGearState, _iValue);
-            return true;
+            throw new NotImplementedException();
+        }
+
+        public int 取最大血量(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取最大魔量(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取格檔值(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取爆擊傷害(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取爆擊率(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取生命回復(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取移動速度(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取血量加乘(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取金幣拾取量(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取防禦力(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取防禦力加乘(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int 取魔量加乘(int _iValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設攻擊()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設攻擊力加乘()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設攻擊速度()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設最大血量()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設最大魔量()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設格檔值()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設爆擊傷害()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設爆擊率()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設生命回復()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設移動速度()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設血量加乘()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設金幣拾取量()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設防禦力()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設防禦力加乘()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool 設魔量加乘()
+        {
+            throw new NotImplementedException();
         }
     }
 }
