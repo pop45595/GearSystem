70c7a447405951dbe8a1a47bda4da5608a7480ef
Yang <repeatsa01@gmail.com>
2017-07-27 21:49:34 +0800

    修改狀態單  我給相反了

--- GearSystem/Interface/IProperty.cs
+++ GearSystem/Interface/IProperty.cs
@@ -3,20 +3,20 @@ namespace GearSystem
 {
     public interface IProperty
     {
-        int 取攻擊(int _iValue);
-        int 取攻擊力加乘(int _iValue);
-        int 取防禦力(int _iValue);
-        int 取防禦力加乘(int _iValue);
-        int 取最大血量(int _iValue);
-        int 取血量加乘(int _iValue);
-        int 取最大魔量(int _iValue);
-        int 取魔量加乘(int _iValue);
-        int 取移動速度(int _iValue);
-        int 取生命回復(int _iValue);
-        int 取攻擊速度(int _iValue);
-        int 取爆擊率(int _iValue);
-        int 取爆擊傷害(int _iValue);
-        int 取格檔值(int _iValue);
-        int 取金幣拾取量(int _iValue);
+        int 取攻擊();
+        int 取攻擊力加乘();
+        int 取防禦力();
+        int 取防禦力加乘();
+        int 取最大血量();
+        int 取血量加乘();
+        int 取最大魔量();
+        int 取魔量加乘();
+        int 取移動速度();
+        int 取生命回復();
+        int 取攻擊速度();
+        int 取爆擊率();
+        int 取爆擊傷害();
+        int 取格檔值();
+        int 取金幣拾取量();
     }
 }
--- GearSystem/Interface/IPropertyV2.cs
+++ GearSystem/Interface/IPropertyV2.cs
@@ -3,21 +3,21 @@ namespace GearSystem
 {
     public interface IPropertyV2 : IProperty
     {
-        bool 設攻擊();
-        bool 設攻擊力加乘();
-        bool 設防禦力();
-        bool 設防禦力加乘();
-        bool 設最大血量();
-        bool 設血量加乘();
-        bool 設最大魔量();
-        bool 設魔量加乘();
-        bool 設移動速度();
-        bool 設生命回復();
-        bool 設攻擊速度();
-        bool 設爆擊率();
-        bool 設爆擊傷害();
-        bool 設格檔值();
-        bool 設金幣拾取量();
+        bool 設攻擊(int _iValue);
+        bool 設攻擊力加乘(int _iValue);
+        bool 設防禦力(int _iValue);
+        bool 設防禦力加乘(int _iValue);
+        bool 設最大血量(int _iValue);
+        bool 設血量加乘(int _iValue);
+        bool 設最大魔量(int _iValue);
+        bool 設魔量加乘(int _iValue);
+        bool 設移動速度(int _iValue);
+        bool 設生命回復(int _iValue);
+        bool 設攻擊速度(int _iValue);
+        bool 設爆擊率(int _iValue);
+        bool 設爆擊傷害(int _iValue);
+        bool 設格檔值(int _iValue);
+        bool 設金幣拾取量(int _iValue);
         IPropertyV2 copy();
     }
 }
