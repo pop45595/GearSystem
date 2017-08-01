ba7310714d023177e26a09eec0e0857ac1e0a2c9
Yang <repeatsa01@gmail.com>
2017-08-01 23:21:21 +0800

    重新更改過interface的概念

--- GearSystem/Interface/IPropertyV2.cs
+++ GearSystem/Interface/IPropertyV2.cs
@@ -3,22 +3,7 @@ namespace GearSystem
 {
     public interface IPropertyV2 : IProperty
     {
-        bool 設攻擊(int _iValue);
-        bool 設攻擊力加乘(int _iValue);
-        bool 設防禦力(int _iValue);
-        bool 設防禦力加乘(int _iValue);
-        bool 設最大血量(int _iValue);
-        bool 設血量加乘(int _iValue);
-        bool 設最大魔量(int _iValue);
-        bool 設魔量加乘(int _iValue);
-        bool 設移動速度(int _iValue);
-        bool 設生命回復(int _iValue);
-        bool 設攻擊速度(int _iValue);
-        bool 設爆擊率(int _iValue);
-        bool 設爆擊傷害(int _iValue);
-        bool 設格檔值(int _iValue);
-        bool 設金幣拾取量(int _iValue);
-        void reset();
+        int getValue(e狀態名 _eKey );
         IPropertyV2 copy();
     }
 }
