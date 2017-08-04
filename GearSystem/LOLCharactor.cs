177113486f7bdb2d8d5903c7afd2b66645f80bbd
Kemeter <pop45595@gmail.com>
2017-08-04 12:05:13 +0800

    更新

--- GearSystem/LOLCharactor.cs
+++ GearSystem/LOLCharactor.cs
@@ -2,60 +2,60 @@
 using System.Collections.Generic;
 using GearSystem;
 
-namespace GearSystem { 
-    public class LOLCharactor : ICharactor<GearInfo>
+
+public class LOLCharactor : ICharactor<GearInfo>
+{
+    private IPropertyV2 m_charactorProperty = null;
+    private IStateMgr m_stateMgr = null;
+    //private ICreator<IGear> m_GearCreator = null;
+    private List<int> m_iListGearIDRecord = null;
+    private int m_iMaxGear = 6;
+    public LOLCharactor() {
+        m_iListGearIDRecord = new List<int>();
+        m_charactorProperty = new PropertyForm();
+        m_stateMgr = new StateMgr(m_iMaxGear);
+    }
+    public IProperty getProperty()
     {
-        private IPropertyV2 m_charactorProperty = null;
-        private IStateMgr m_stateMgr = null;
-        //private ICreator<IGear> m_GearCreator = null;
-        private List<int> m_iListGearIDRecord = null;
-        private int m_iMaxGear = 6;
-        public LOLCharactor() {
-            m_iListGearIDRecord = new List<int>();
-            m_charactorProperty = new PropertyForm();
-            m_stateMgr = new StateMgr(m_iMaxGear);
-        }
-        public IProperty getProperty()
-        {
-            return m_stateMgr.getProperty();
-        }
+        return m_stateMgr.getProperty();
+    }
 
-        public bool addGear(GearInfo _addGearInfo) {
-            IGear gear = GearCreator.Create(_addGearInfo.m_GearId);
-            if (m_stateMgr.addGear(gear, m_iListGearIDRecord.Count)) {
-                int iGearId = _addGearInfo.m_GearId;
-                m_iListGearIDRecord.Add(iGearId);
-                return true;
-            } else {
-                return false;
-            }
-
-        }
-
-        public bool removeGear(GearInfo _delGearInfo) {
-            int? iGearPosition = getGearPosition(_delGearInfo.m_GearId);
-            if (iGearPosition.HasValue) {
-                if (m_stateMgr.removeGear(iGearPosition.Value)) {
-                    m_iListGearIDRecord.RemoveAt(iGearPosition.Value);
-                    return true;
-                }
-            }
+    public bool addGear(GearInfo _addGearInfo) {
+        IGear gear = GearCreator.Create(_addGearInfo.m_GearId);
+        if (m_stateMgr.addGear(gear, m_iListGearIDRecord.Count)) {
+            int iGearId = _addGearInfo.m_GearId;
+            m_iListGearIDRecord.Add(iGearId);
+            return true;
+        } else {
             return false;
-        }
-        private int? getGearPosition(int _delGearID) {
-            int iTargetID = _delGearID;
-            for (int i = 0; i < m_iListGearIDRecord.Count; ++i) {
-                if (m_iListGearIDRecord[i] == iTargetID) {
-                    return i;
-                }
-            }
-            return null;
-        }
-        public void setPropertyUpdateCallBack(reflashCallback _callBack)
-        {
-            m_stateMgr.setReflashCallback(_callBack);
         }
 
     }
+
+    public bool removeGear(GearInfo _delGearInfo) {
+        int? iGearPosition = getGearPosition(_delGearInfo.m_GearId);
+        if (iGearPosition.HasValue) {
+            if (m_stateMgr.removeGear(iGearPosition.Value)) {
+                m_iListGearIDRecord.RemoveAt(iGearPosition.Value);
+                return true;
+            }
+        }
+        return false;
+    }
+    private int? getGearPosition(int _delGearID) {
+        int iTargetID = _delGearID;
+        for (int i = 0; i < m_iListGearIDRecord.Count; ++i) {
+            if (m_iListGearIDRecord[i] == iTargetID) {
+                return i;
+            }
+        }
+        return null;
+    }
+    public void setPropertyUpdateCallBack(reflashCallback _callBack)
+    {
+        m_stateMgr.setReflashCallback(_callBack);
+    }
+
 }
+
 
--- GearSystem/Program.cs
+++ GearSystem/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-
-
 using GearSystem;
 
 class Program
@@ -9,27 +7,32 @@ class Program
     {
         ICharactor<GearInfo> lolChractor = new LOLCharactor();
         IProperty lolProperty = lolChractor.getProperty();
+        showInfo(lolProperty);
         lolChractor.setPropertyUpdateCallBack(delegate {
-            Console.WriteLine("攻擊力 : " + lolProperty.getValueToFloat(e狀態名.攻擊));
-            Console.WriteLine("攻擊力加乘 : " + lolProperty.getValueToFloat(e狀態名.攻擊力加乘));
-            Console.WriteLine("防禦力 : " + lolProperty.getValueToFloat(e狀態名.防禦力)); 
-            Console.WriteLine("防禦力加乘 : " + lolProperty.getValueToFloat(e狀態名.防禦力加乘));
-            Console.WriteLine("最大血量 : " + lolProperty.getValueToFloat(e狀態名.最大血量));
-            Console.WriteLine("血量加乘 : " + lolProperty.getValueToFloat(e狀態名.血量加乘));
-            Console.WriteLine("最大魔量 : " + lolProperty.getValueToFloat(e狀態名.最大魔量));
-            Console.WriteLine("魔量加乘 : " + lolProperty.getValueToFloat(e狀態名.魔量加乘));
-            Console.WriteLine("移動速度 : " + lolProperty.getValueToFloat(e狀態名.移動速度));
-            Console.WriteLine("生命回復 : " + lolProperty.getValueToFloat(e狀態名.生命回復));
-            Console.WriteLine("攻擊速度 : " + lolProperty.getValueToFloat(e狀態名.攻擊速度));
-            Console.WriteLine("爆擊率 : " + lolProperty.getValueToFloat(e狀態名.暴擊率));
-            Console.WriteLine("爆擊傷害 : " + lolProperty.getValueToFloat(e狀態名.暴擊傷害));
-            Console.WriteLine("格檔值 : " + lolProperty.getValueToFloat(e狀態名.格擋值));
-            Console.WriteLine("金幣拾取量 : " + lolProperty.getValueToFloat(e狀態名.金幣時取量));
+            lolProperty = lolChractor.getProperty();
+            showInfo(lolProperty);
         });
         lolChractor.addGear(new GearInfo(0));
-        Console.WriteLine("");
         lolChractor.addGear(new GearInfo(1));
         Console.ReadLine(); // 暫停，沒寫則會很快關閉程式
+    }
+    private static void showInfo(IProperty _lolProperty) {
+        Console.WriteLine("攻擊力 : " + _lolProperty.getValueToFloat(e狀態名.攻擊));
+        Console.WriteLine("攻擊力加乘 : " + _lolProperty.getValueToFloat(e狀態名.攻擊力加乘));
+        Console.WriteLine("防禦力 : " + _lolProperty.getValueToFloat(e狀態名.防禦力));
+        Console.WriteLine("防禦力加乘 : " + _lolProperty.getValueToFloat(e狀態名.防禦力加乘));
+        Console.WriteLine("最大血量 : " + _lolProperty.getValueToFloat(e狀態名.最大血量));
+        Console.WriteLine("血量加乘 : " + _lolProperty.getValueToFloat(e狀態名.血量加乘));
+        Console.WriteLine("最大魔量 : " + _lolProperty.getValueToFloat(e狀態名.最大魔量));
+        Console.WriteLine("魔量加乘 : " + _lolProperty.getValueToFloat(e狀態名.魔量加乘));
+        Console.WriteLine("移動速度 : " + _lolProperty.getValueToFloat(e狀態名.移動速度));
+        Console.WriteLine("生命回復 : " + _lolProperty.getValueToFloat(e狀態名.生命回復));
+        Console.WriteLine("攻擊速度 : " + _lolProperty.getValueToFloat(e狀態名.攻擊速度));
+        Console.WriteLine("爆擊率 : " + _lolProperty.getValueToFloat(e狀態名.暴擊率));
+        Console.WriteLine("爆擊傷害 : " + _lolProperty.getValueToFloat(e狀態名.暴擊傷害));
+        Console.WriteLine("格檔值 : " + _lolProperty.getValueToFloat(e狀態名.格擋值));
+        Console.WriteLine("金幣拾取量 : " + _lolProperty.getValueToFloat(e狀態名.金幣時取量));
+        Console.WriteLine("");
     }
 }
 
