6ae01c1422e0628b3a3ef35c60361870b9bf7732
Kemeter <pop45595@gmail.com>
2017-08-18 00:25:56 +0800

    UPUP

---
+++ GearSystem/StateUpdateInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GearSystem {
+    public class StateUpdateInfo {
+        public int m_iGearId = 0;
+        public int m_iStateId = 0;
+        public int m_iUpdateUnit = 0;
+        public StateUpdateInfo(int _iGearId, int _iStateId, int _iUpdateUnit) {
+            m_iGearId = _iGearId;
+            m_iStateId = _iStateId;
+            m_iUpdateUnit = _iUpdateUnit;
+        }
+    }
+}
